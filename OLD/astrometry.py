193f6a07f1b75fbc4deddb9a84e39108bdd0a084
tony2heads <tony@ska.ac.za>
2017-05-03 16:44:00 +0200

    Save from loss

---
+++ OLD/astrometry.py
@@ -0,0 +1,38 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from pyslalib import slalib
+slalib.sla_veri()
+
+
+# In[2]:
+
+slalib.sla_caldj(1999, 12, 31)
+
+
+# In[3]:
+
+slalib.sla_etrms(1976.9)
+
+
+# In[4]:
+
+slalib.sla_fk45z(1.234, -0.123, 1984)
+
+
+# In[5]:
+
+slalib.sla_dafin("-00 03 34.6", 1)
+
+
+# In[6]:
+
+slalib.sla_obs(0, "GBT")
+
+
+# In[ ]:
+
+
+
